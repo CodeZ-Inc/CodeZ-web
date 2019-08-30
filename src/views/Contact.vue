23a603f6d3a501e14c5a3b55371ae9ec1b1e2d84
Ravi Anand <ravi.anand@nbt.ag>
2019-08-30 18:19:19 +0200

    Remove required validation for now

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -45,14 +45,14 @@
 
 <script>
 import { validationMixin } from 'vuelidate'
-import { required, maxLength, email } from 'vuelidate/lib/validators'
+import { maxLength, email } from 'vuelidate/lib/validators'
 
 export default {
   name: 'Contact',
   mixins: [validationMixin],
   validations: {
-    name: { required, maxLength: maxLength(100) },
-    email: { required, email },
+    name: { maxLength: maxLength(100) },
+    email: { email },
     message: { maxLength: maxLength(2024) }
   },
 
@@ -70,14 +70,14 @@ export default {
       const errors = []
       if (!this.$v.name.$dirty) return errors
       !this.$v.name.maxLength && errors.push('Name must be at most 100 characters long')
-      !this.$v.name.required && errors.push('Name is required.')
+      // !this.$v.name.required && errors.push('Name is required.')
       return errors
     },
     emailErrors () {
       const errors = []
       if (!this.$v.email.$dirty) return errors
       !this.$v.email.email && errors.push('Must be valid e-mail')
-      !this.$v.email.required && errors.push('E-mail is required')
+      // !this.$v.email.required && errors.push('E-mail is required')
       return errors
     },
     messageErrors () {
