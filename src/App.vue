ebcdd363417b03ba2fd86bb26a9015240af5d8da
Ravi Anand <ravi.anand@nbt.ag>
2019-09-07 12:33:18 +0200

    Add production banner

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <v-app>
-    <nav-header/>
-    <v-content>
-      <router-view/>
-    </v-content>
-    <nav-footer/>
+    <v-container v-if="env === 'production'" fill-height grid-list-md class="text-sm-center black">
+      <v-layout row wrap align-center>
+        <v-flex>
+          <h1 class="display-3 white--text"> We are launching soon!
+          </h1>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <div v-else>
+      <nav-header/>
+      <v-content>
+        <router-view/>
+      </v-content>
+      <nav-footer/>
+    </div>
   </v-app>
 </template>
 
@@ -17,9 +27,19 @@ export default {
   components: {
     NavFooter,
     NavHeader
+  },
+  data () {
+    return {
+      env: 'production'
+    }
   }
 }
 </script>
 
 <style lang="scss">
+.div-production {
+  background-color: yellow;
+  height: 100%;
+  font-weight: bold;
+}
 </style>
