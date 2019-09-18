7906b45ee5ca6ef40a2c891e031dc7ef33b7f4a9
Ravi <ravianand1988@live.com>
2019-09-18 17:32:40 +0200

    Add NavHeader back

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
   <v-app>
-    <nav-header/>
     <v-content>
       <router-view/>
     </v-content>
-    <nav-footer/>
   </v-app>
 </template>
 
 <script>
-import NavHeader from '@/components/NavHeader'
-import NavFooter from '@/components/NavFooter'
+// import NavHeader from '@/components/NavHeader'
+// import NavFooter from '@/components/NavFooter'
 
 export default {
   name: 'App',
   components: {
-    NavFooter,
-    NavHeader
   }
 }
 </script>
---
+++ src/components/NavHeader.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-toolbar color="secondary" class="elevation-0">
+    <v-toolbar-title v-text="title"></v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn
+      v-for="link in links"
+      :key="link.text"
+      color="primary"
+      text
+      rounded
+      class="my-2"
+      :href="link.path"
+      @click="setActive(link.path)"
+    >
+      <span :class="{'font-weight-bold': isActiveLink === link.path}">{{ link.text }}</span>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'NavHeader',
+  data () {
+    return {
+      title: 'CodeZ',
+      links: [
+        {
+          text: 'Our Products',
+          path: '#products'
+        },
+        {
+          text: 'Team',
+          path: '#team'
+        },
+        {
+          text: 'About Us',
+          path: '#about'
+        },
+        {
+          text: 'Contact Us',
+          path: '#contact'
+        }
+      ]
+    }
+  },
+  computed: {
+    ...mapState(['activeLink']),
+    isActiveLink () {
+      return this.activeLink === undefined ? this.$router.currentRoute.hash : this.activeLink
+    }
+  },
+  methods: {
+    ...mapActions(['setActiveLink']),
+    setActive (link) {
+      this.setActiveLink(link)
+      if (link === '') {
+        this.scrollToTop()
+      }
+    },
+    scrollToTop () {
+      document.body.scrollTop = 0 // For Safari
+      document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
